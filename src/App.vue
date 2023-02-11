034513cf341e36cab55c1950f2b0a6f9ed5d4a59
Denis Lozko <denis.lozko@cirus-it.com>
2023-02-11 21:39:05 +0300

    Added smooth scroll

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,44 @@
 <template>
-  <RouterView />
+  <RouterView v-if="isTouchScreen" />
+  <Scrollbar v-else>
+    <RouterView />
+  </Scrollbar>
 </template>
 
 <script setup>
-  import { RouterView } from "vue-router";
+  import { ref, watch, onMounted } from 'vue';
+  import Scrollbar from 'vue3-smooth-scrollbar';
+  import { RouterView } from 'vue-router';
+
+  const isTouchScreen = ref(null);
+
+  const CheckTouchScreen = () => {
+    isTouchScreen.value =
+      'ontouchstart' in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0;
+  };
+  CheckTouchScreen();
+
+  const toggleScroll = (value) => {
+    if (value) {
+      body.classList.remove('lock');
+      app.classList.remove('smooth-scroll');
+    } else {
+      body.classList.add('lock');
+      app.classList.add('smooth-scroll');
+    }
+  };
+
+  onMounted(() => {
+    const body = document.body;
+    const app = document.getElementById('app');
+    toggleScroll(isTouchScreen.value);
+  });
+
+  watch(isTouchScreen, (newValue) => {
+    toggleScroll(newValue);
+  });
 </script>
 
 <style scoped>
